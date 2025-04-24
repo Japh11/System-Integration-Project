c12237a38b384cd828419fa3388fcb62208acfea
Japh11 <lirasanjaphet@gmail.com>
2025-04-25 00:05:55 +0800

    update sec

--- IskoLAIR/src/main/java/com/Capstone/IskoLAIR/Security/SecurityConfig.java
+++ IskoLAIR/src/main/java/com/Capstone/IskoLAIR/Security/SecurityConfig.java
@@ -124,7 +124,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of(
+        	    "http://localhost:5173",
+        	    "https://system-integration-project.vercel.app"
+        	));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
